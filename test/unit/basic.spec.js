09614ab21c14ef757a073fa215e07f250cc076d1
itayw <itay@joo.la>
2014-03-02 09:53:30 +0200

    #3 added support for date parsing

--- test/unit/basic.spec.js
+++ test/unit/basic.spec.js
@@ -6,6 +6,8 @@ describe('given an object', function () {
       number: 1,
       string: 'string',
       date: new Date(),
+      datestr: 'Sun Mar 02 2014 09:48:11 GMT+0200 (IST)',
+      anotherdatestr: '2014-03-02T07:48:11',
       array: [1, '2', [1, 2]],
       nested: {
         number: 1,
@@ -16,8 +18,15 @@ describe('given an object', function () {
   });
 
   it("should describe a JSON object", function () {
-    var _obj = dj.describe(this.obj);
-    expect(_obj).to.be.ok;
+    var expected = { number: 'number',
+      string: 'string',
+      date: 'date',
+      datestr: 'date',
+      anotherdatestr: 'date',
+      array: [ 'number', 'date', [ 'number', 'number' ] ],
+      nested: { number: 'number', string: 'string', date: 'date' } };
+    var actual = dj.describe(this.obj);
+    assert.deepEqual(actual, expected, 'Failed to describe JSON object correctly');
   });
 
   it("should flatten JSON object", function () {
@@ -34,8 +43,12 @@ describe('given an object', function () {
         bb: ['cc']
       }
     };
-    var _obj = dj.flatten(obj);
-    expect(_obj).to.be.ok;
-    console.log(_obj);
+    var expected = [
+      [ 'level0', 1 ],
+      [ 'a.b.c.d', 1 ],
+      [ 'aa.bb', [ 'cc' ] ]
+    ];
+    var actual = dj.flatten(obj);
+    assert.deepEqual(actual, expected, 'Failed to flatten JSON object correctly');
   });
 });
